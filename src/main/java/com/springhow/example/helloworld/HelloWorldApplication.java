ccdf214c8af175fda2cb2fe93abb16e9cf132da0
Pratik <63726926+Pratik-7664@users.noreply.github.com>
2024-03-14 10:58:33 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello From Unnati!";
+        return "Hello  world!";
     }
 
 }
